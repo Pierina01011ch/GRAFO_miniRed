a8415b39fb42d006eb9dafde1ff30b97c1a53d0a
Pierina01011ch <pierina21op15A@gmail.com>
2026-06-08 02:27:16 -0500

    Mini Red Social

---
+++ MiniRedSocial_/MiniRedSocial/GrafoNPND.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniRedSocial
+{
+    internal class GrafoNPND
+    {
+        private int[,] red;
+
+        public GrafoNPND(int cantPers) {
+            red = new int[cantPers, cantPers];
+        }
+
+        public void Conectar(int orig, int dest) { //Conexión ND de Amistad Social
+            red[orig, dest] = 1;
+            red[dest, orig] = 1;
+        }
+
+        public bool SonAmigos(int orig, int dest, int cant) //Recorrido en amplitud BFS
+        {
+            bool[] visitado = new bool[cant];
+            Queue<int> cola = new Queue<int>();
+
+            visitado[orig] = true;
+            cola.Enqueue(orig);
+
+            while (cola.Count>0)
+            {
+                int actual = cola.Dequeue();
+                if (actual == dest) return true;
+
+                for (int i=0; i<cant; i++)
+                {
+                    if (red[actual, i] == 1 && !visitado[i]) {
+                        visitado[i] = true;
+                        cola.Enqueue(i);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ MiniRedSocial_/MiniRedSocial/Program.cs
@@ -0,0 +1,113 @@
+﻿using MiniRedSocial;
+
+namespace MiniRedSocial
+{
+    internal class Program
+    {
+        static Persona[] lista = new Persona[100];
+        static int cant = 0;
+
+        static GrafoNPND redSocial = new GrafoNPND(100);
+
+        static void Main(string[] args)
+        {
+            int op;
+
+            do
+            {
+                Console.WriteLine("\n=====MINI RED SOCIAL=====");
+                Console.WriteLine("1. Agregar Persona");
+                Console.WriteLine("2. Conecta Personas");
+                Console.WriteLine("3. ¿Son amigos?");
+                Console.WriteLine("4. Salir");
+
+                Console.Write("Digite el # de opción: ");
+                op = int.Parse(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1: AgregarPersona(); break;
+                    case 2: ConectaPersonas(); break;
+                    case 3: VerificarAmistad(); break;
+                }
+            } while (op != 4);
+        }
+
+        static void AgregarPersona()
+        {
+            Persona p = new Persona();
+
+            Console.Write("Nombre: ");
+            p.Nombre = Console.ReadLine();
+
+            Console.Write("Género: ");
+            p.Genero = Console.ReadLine();
+
+            Console.Write("Edad: ");
+            p.Edad = int.Parse(Console.ReadLine());
+
+            lista[cant] = p;
+            cant++;
+
+            Console.WriteLine("\n---PERSONA REGISTRADA---");
+            Console.WriteLine($"Nombre: {p.Nombre}");
+            Console.WriteLine($"Género: {p.Genero}");
+            Console.WriteLine($"Edad: {p.Edad} años");
+        }
+
+        static int Buscar(string nombre)
+        {
+            for (int i = 0; i < cant; i++)
+            {
+                if (lista[i].Nombre == nombre)
+                    return i;
+            }
+            return -1;
+        }
+
+        static void ConectaPersonas()
+        {
+            Console.Write("Persona 1: ");
+            string p1 = Console.ReadLine();
+
+            Console.Write("Persona 2: ");
+            string p2 = Console.ReadLine();
+
+            int i = Buscar(p1);
+            int j = Buscar(p2);
+
+            if (i == -1 || j == -1)
+            {
+                Console.WriteLine("Persona no encontrada");
+                return;
+            }
+
+            redSocial.Conectar(i, j);
+
+            Console.WriteLine("Conexión creada");
+        }
+
+        static void VerificarAmistad()
+        {
+            Console.Write("Persona 1: ");
+            string p1 = Console.ReadLine();
+
+            Console.Write("Persona 2: ");
+            string p2 = Console.ReadLine();
+
+            int i = Buscar(p1);
+            int j = Buscar(p2);
+
+            if (i == -1 || j == -1)
+            {
+                Console.WriteLine("Persona no encontrada");
+                return;
+            }
+
+            if (redSocial.SonAmigos(i, j, cant))
+                Console.WriteLine("Sí están relacionados");
+            else
+                Console.WriteLine("No están relacionados");
+        }
+    }
+}
